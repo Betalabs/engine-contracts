ff6d62230ee0ef24e7472886f664bc588bc2bb73
Victor Macedo de Moura <victor.macedo@betalabs.com.br>
2019-01-17 16:13:18 -0200

    Update PaymentMethod.php

--- src/PaymentMethod.php
+++ src/PaymentMethod.php
@@ -4,5 +4,52 @@ namespace Betalabs\Engine\Contracts;
 
 interface PaymentMethod
 {
-    //
+    /**
+     * Returns payment method's key.
+     *
+     * @return bool
+     */
+    public function getKey();
+
+    /**
+     * Returns true if type is bank slip.
+     *
+     * @return bool
+     */
+    public function isBankSlip();
+
+    /**
+     * Returns true if type is credit card.
+     *
+     * @return bool
+     */
+    public function isCreditCard();
+
+    /**
+     * Returns true if type is debit card.
+     *
+     * @return bool
+     */
+    public function isDebitCard();
+
+    /**
+     * Returns true if type is credit or debit card.
+     *
+     * @return bool
+     */
+    public function isCard();
+
+    /**
+     * Returns true if type is payment on shipping.
+     *
+     * @return bool
+     */
+    public function isPaymentOnShipping();
+
+    /**
+     * Returns true if type is non transparent checkout.
+     *
+     * @return bool
+     */
+    public function isNonTransparentCheckout();
 }
