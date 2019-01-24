3ee15d053f548173bafa3852e8eb002e87f15ac7
Victor Macedo de Moura <victor.macedo@betalabs.com.br>
2019-01-24 11:28:30 -0200

    Update PaymentGateway.php

--- src/PaymentGateway.php
+++ src/PaymentGateway.php
@@ -4,5 +4,10 @@ namespace Betalabs\Engine\Contracts;
 
 interface PaymentGateway
 {
-    //
+    /**
+     * Get path for integrator class.
+     *
+     * @return string
+     */
+    public function getIntegratorClassPath();
 }
